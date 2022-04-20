c87619690153c4175b791a9e7b408493f56659d5
PedroMarMol <martosmoleropedro@gmail.com>
2022-04-20 09:38:49 +0200

    finished css and html, starting to set up javascript code

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
 const add = (a,b) => a+b;
 
 const subtract = (a,b) => a-b;
@@ -5,3 +13,7 @@ const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
 
 const divide = (a,b) => a/b;
+
+function myFunction(el) {
+    document.getElementById("display").innerHTML = el.innerHTML;
+}
